c96148675a547ed8b3d424390a7b6ea5ca22cf9b
Orlando Mina <65779635+Orloxx23@users.noreply.github.com>
2022-11-15 23:04:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -67,17 +67,17 @@ const httpServer = createServer(app);
 
 // Socket.io
 const io = new Server(httpServer, {
-  cors: {
-    origin: "https://unituit-client.vercel.app"
-    //origin: "http://localhost:3000",
-    // origin: function (origin, callback) {
-    //   if (whitelist.indexOf(origin) > -1) {
-    //     callback(null, true);
-    //   } else {
-    //     callback(null, false);
-    //   }
-    // },
-  },
+  // cors: {
+  //   origin: "https://unituit-client.vercel.app"
+  //   //origin: "http://localhost:3000",
+  //   // origin: function (origin, callback) {
+  //   //   if (whitelist.indexOf(origin) > -1) {
+  //   //     callback(null, true);
+  //   //   } else {
+  //   //     callback(null, false);
+  //   //   }
+  //   // },
+  // },
 });
 
 let onlineUsers = [];
